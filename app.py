c16ff8ea3b457de36cea05baffc8768e18a2478f
Ben chelbi Wissem <Ben_chelbiw@yahoo.com>
2022-11-22 23:25:49 +0100

    ml

--- app.py
+++ app.py
@@ -1,55 +1,64 @@
-#import os
-#from pathlib import Path
-#import streamlit as st
+import numpy as np
+import streamlit as st
+from PIL import Image, ImageOps
+import tensorflow as tf
 
-from flask import Flask, jsonify, render_template, request
+model = tf.keras.models.load_model('my_model.hdf5')
 
-#import src.utils as utils
+def import_and_predict(image_data, model):
+    
+        size = (75,75)    
+        image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+        image = image.convert('RGB')
+        image = np.asarray(image)
+        image = (image.astype(np.float32) / 255.0)
 
+        img_reshape = image[np.newaxis,...]
 
-#from keras.models import load_model
+        prediction = model.predict(img_reshape)
+        
+        return prediction
+        
+# same idea for dice and jaccard metrics
+def dice_metric(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    score = (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return score
 
-#import segmentation_models as sm
-#from segmentation_models import Unet
-#from segmentation_models import get_preprocessing
+def dice_loss(y_true, y_pred):
+    loss = 1 - dice_metric(y_true, y_pred)
+    return loss
 
-#DATASET_PATH = "C:/Users/jketk/Desktop/p8data"
+model = tf.keras.models.load_model('model.hdf5')
 
+st.write("""
+         # Cancer Prediction
+         """
+         )
 
+st.write("This is a simple image classification web app to predict cancer")
 
-#Create an app object using the Flask class. 
-app = Flask(__name__)
-
-#Add reference fingerprint. 
-#Cookies travel with a signature that they claim to be legit. 
-#Legitimacy here means that the signature was issued by the owner of the cookie.
-#Others cannot change this cookie as it needs the secret key. 
-#It's used as the key to encrypt the session - which can be stored in a cookie.
-#Cookies should be encrypted if they contain potentially sensitive information.
-
-
-
-
-
-
-
-
-#home page
-@app.route('/', methods = ['GET'])
-def home_page():
-    return flask.render_template('index.html')
-
-
-
-if __name__ == "__main__":
-    app.run(host = '0.0.0.0', port = 5000)
-# In[ ]:
-
-
-
-
-
-# In[ ]:
+file = st.file_uploader("Please upload an image file", type=["jpg", "png"])
+#
+if file is None:
+    st.text("You haven't uploaded an image file")
+else:
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image, model)
+    
+    if np.argmax(prediction) == 0:
+        st.write("Not sick")
+    elif np.argmax(prediction) == 1:
+        st.write("Sick")
+    else:
+        st.write("")
+    
+    st.text("Probability (0: Not sick, 1: Sick)")
+    st.write(prediction)
 
 
 
